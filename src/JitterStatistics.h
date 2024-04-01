76a88330eaa1775f8ea58bb6e7d0b50d0ad9f90d
SwatKat1977 <paulmorriss1977@gmail.com>
2024-04-01 18:47:48 +0100

    added start jitter stats

--- src/JitterStatistics.h
+++ src/JitterStatistics.h
@@ -41,14 +41,39 @@ struct FrameJitterEntry {
     double late_;
 };
 
+// Data structure for thread start time jitter.
+struct ThreadStartTimeJitterData {
+    // The current start time.
+    double current_start_time_;
+
+    // The average start time.
+    double average_start_time_;
+
+    // The best start time.
+    double best_start_time_;
+
+    // The worst start time.
+    double  worst_start_time_;
+
+    // accumulated start times
+    double total_start_time_;
+
+    // The total number of passes that have run.
+    unsigned int total_passes_run_;
+};
+
 struct JitterStatistics {
+
+    // ===========================
+    // = Frame Jitter Statistics =
+    // ===========================
     FrameJitterEntry jitter_entries_[MAX_FRAMES];
 
-    // The absolute worst (latest) jitter of any frame to date.
-    double absolute_worst_frame_jitter_;
+    // The worst (latest) jitter of any frame to date.
+    double worst_frame_jitter_;
 
-    // The absolute best (earliest) jitter of any frame to date.
-    double absolute_best_frame_jitter_;
+    // The best (earliest) jitter of any frame to date.
+    double best_frame_jitter_;
 
     // The accumulate total jitter to date.
     double accumulate_total_jitter_;
@@ -59,10 +84,21 @@ struct JitterStatistics {
     // Flag to indicate that this is the first pass through of the jitter
     // calculations. This is done because at least two sets of delta values are
     // required for the jitter calculation.
-    bool first_jitter_calc_pass;
+    bool first_jitter_calc_pass_;
 
     // The number of times jitter data has been calculated.
-    DWORD  jitter_calculation_count;
+    DWORD  jitter_calculation_count_;
+
+    // =======================================
+    // = Thread Start Time Jitter Statistics =
+    // =======================================
+    ThreadStartTimeJitterData thread_start_jitter_data_[MAX_FRAMES];
+
+    // The worst start jitter time to date.
+    double worst_start_jitter_;
+
+    // The best start jitter time to date.
+    double best_start_jitter_;
 };
 
 #endif  // JITTERSTATISTICS_H_
